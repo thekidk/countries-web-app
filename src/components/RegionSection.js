85e63295b912d532f4509cd0c8f5fd6663e38225
Kidane Mussie <kidanemussie@Kidanes-MacBook-Pro.local>
2020-09-21 15:36:33 -0700

    added UI for search

--- src/components/RegionSection.js
+++ src/components/RegionSection.js
@@ -8,6 +8,18 @@ function RegionSection() {
         <div className='region-container'>
             <video src='/video-2.mp4' autoPlay loop muted /> 
             <h1>Select a Region</h1>
+            <br/>
+            <br/>
+            <br/>
+            <li className="container">
+                <input id="search"
+                     class="search"
+                     type="text"
+                     placeholder="Search for a country"
+                />
+            </li>
+            <br/>
+            {<Button buttonStyle='btn--outline'>Search</Button>}
         </div>
     )
 }
